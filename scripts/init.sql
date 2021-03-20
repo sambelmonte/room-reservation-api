4fd97a1262751ca2ae2b5c129046d144fe53f723
Sam Belmonte <torchbearerjp@gmail.com>
2021-03-20 15:16:16 +0800

    Initial commit

---
+++ scripts/init.sql
@@ -0,0 +1,47 @@
+CREATE TABLE IF NOT EXISTS `admin_users`
+(
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `hash` varchar(50) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `deleted` bit NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `regular_users`
+(
+  `id`         int unsigned NOT NULL AUTO_INCREMENT,
+  `name`       varchar(50) NOT NULL,
+  `hash`       varchar(100) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `deleted`    bit NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `rooms`
+(
+  `id`           int unsigned NOT NULL AUTO_INCREMENT ,
+  `name`         varchar(50) NOT NULL ,
+  `created_by`   int unsigned NOT NULL ,
+  `created_at`   datetime NOT NULL ,
+  `deleted`      bit NOT NULL DEFAULT 0 ,
+  `max_capacity` int unsigned NOT NULL ,
+
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`created_by`) REFERENCES `admin_users` (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `reservations`
+(
+  `id`           int unsigned NOT NULL AUTO_INCREMENT ,
+  `room_id`      int unsigned NOT NULL ,
+  `user_id`      int unsigned NOT NULL ,
+  `start_time`   datetime NOT NULL ,
+  `end_time`     datetime NOT NULL ,
+  `cancelled`    bit NOT NULL DEFAULT 0 ,
+  `people_count` int NOT NULL ,
+
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`room_id`) REFERENCES `rooms` (`id`),
+  FOREIGN KEY (`user_id`) REFERENCES `regular_users` (`id`)
+);
